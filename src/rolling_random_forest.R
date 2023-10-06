585202eb1cfba11f9c1481dcfc154d4be059c9fa
mbuerg <106337257+mbuerg@users.noreply.github.com>
2023-10-06 21:21:35 +0200

    docstring in rolling window rf eingefügt

--- src/rolling_random_forest.R
+++ src/rolling_random_forest.R
@@ -1,47 +1,75 @@
-# In diesem Skript wird eine Funktion gebaut, die mit einem rolling window
-# immer wieder einen Random Forest fittet und die forecasting errors notiert.
-# Die Gesamtforecastperformance ist dann der MSE aller Fehler
-
 library(data.table)
 library(ranger)
+#library(roxygen2)
+library(docstring)
+#library(rstudioapi)
 
-premiums <- fread(path_data_premium_omitted)
+#premiums <- fread(path_data_premium_omitted)
 
 
-# Definiere Funktion mit rolling window und Anwendung eines Random Forest
-# speichere den error ab, bevor das Fenster weiterrollt
-
-rolling_random_forest <- function(data, window_size, h, response){
-  # data ist das sample der Zeitreihen
-  # window_size ist die Länge des Fensters
-  # h ist der forecast horizon
-  # reponse ist die abhängige Variable
-  # gebe Fehler, mse und den letzten random forest aus. Den letzten Random Forest
-  # kann man dann zur Vorhersage verwenden
+rolling_window_rf_walk_forward <- function(data
+                                           , window_size){
+  #' Rolling Window Random Forest with walk-forward validation
+  #' 
+  #' @description This function rolls over time series data and fits a 
+  #' random forest on the window. After every window the fit is tested on the
+  #' very next observation. The window continues to roll until there is no more
+  #' test data left.
+  #' 
+  #' After this process was finished the MSFE is calculated that is the 
+  #' MSE of the errors calculated on the very next observations after every
+  #' window.
+  #' 
+  #' @param data dataframe with response and all features
+  #' @param window_size size of the rolling window
+  #' 
+  #' @usage rolling_window_rf_walk_forward(data, window_size)
+  #' 
+  #' @return Returns every error of the individual test errors, the msfe,
+  #' the ranger fit and all predictions made on the testdata
+  #' 
+  #' @details In order to use this function properly the response variable has
+  #' to be shifted according to the forecast horizon you are interested in.
+  #' eg if you are interested in forecasting 3 steps ahead you have to make
+  #' sure your data can be used to estimate the model 
+  #' r_{t+3} = F_{t} + epsilon_{t+3}
+  #' 
+  #' @examples
+  #' rolling_window_rf_walk_forward(premium_data, 10000) creates a rolling window of size 10000
   
-  # benenne response Variable entsprechend um
-  names(data)[names(data) == response] <- "response"
   
-  error = c() # Hier drin sollen forecast errors gesammelt werden
+  #set.seed(123)
   
-  # Anzahl der windows ist T - window_size - h + 1
-  amount_of_windows <- data[, .N] - window_size - h + 1
+  error <- c()
+  predictions <- c()
   
-  for(i in 1:amount_of_windows){
-    # lasse windows laufen bis zum letzten und berichte die errors
+  for(i in 1:(nrow(data) - window_size)){
     
-      window = data[i:((window_size+i)-1)]
-      
-      random_forest_fit <- ranger(response ~.,
-                                  data = window)
-      
-      prediction <- predict(random_forest_fit,
-                            data = data[window_size+i+h-1, ])
-      
-      rf_error <- data[window_size+i+h-1, response] - prediction$predictions
-      
-      error <- append(error, rf_error) 
+    #set.seed(i)
+    
+    window <- data[i:(i+window_size)-1, ]
+    
+    rf_fit <- ranger(prem ~.
+                     , data=window
+                     #, num.trees=200
+                     #, replace=FALSE
+                     #, sample.fraction=0.632
+                     #, mtry=mtry_split_amount
+                     #, seed=i
+                     )
+    
+    prediction <- predict(rf_fit ,
+                          data=data[(i+window_size), ])
+    
+    residual <- data[(i+window_size), "prem"] - prediction$predictions
+    
+    error <- c(error, residual)
+    predictions <- c(predictions, prediction$predictions)
+    
   }
-  mse <- mean(error^2)
-  return(list(error, mse, random_forest_fit))
+  msfe <- mean(error^2)
+  
+  return(list(error, msfe, rf_fit, predictions))
 }
+
+docstring(rolling_window_rf_walk_forward)
