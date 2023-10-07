b6973c0931e46a4bcc9bd3fbfc6a6673d02fcefc
mbuerg <106337257+mbuerg@users.noreply.github.com>
2023-10-07 12:41:12 +0200

    windows were incorrectly set i:(i+window_size)-1 changed to i:(i+window_size-1)

--- src/rolling_random_forest.R
+++ src/rolling_random_forest.R
@@ -1,18 +1,18 @@
 library(data.table)
 library(ranger)
-#library(roxygen2)
 library(docstring)
-#library(rstudioapi)
 
-#premiums <- fread(path_data_premium_omitted)
-
-
-rolling_window_rf_walk_forward <- function(data
-                                           , window_size){
-  #' Rolling Window Random Forest with walk-forward validation
+rolling_window_rf_walk_forward_ts <- function(data
+                                           , window_size
+                                           , response
+                                           , mtry_split_amount
+                                           , trees = 500
+                                           , s = 0.632
+                                           ){
+  #' Rolling Window Random Forest with walk-forward validation (time series)
   #' 
   #' @description This function rolls over time series data and fits a 
-  #' random forest on the window. After every window the fit is tested on the
+  #' random forest (RF) on the window. After every window the fit is tested on the
   #' very next observation. The window continues to roll until there is no more
   #' test data left.
   #' 
@@ -22,6 +22,11 @@ rolling_window_rf_walk_forward <- function(data
   #' 
   #' @param data dataframe with response and all features
   #' @param window_size size of the rolling window
+  #' @param response is the name of the dependent variable in the data
+  #' @param mtry_split_amount specifies the mtry parameter that gives the amount
+  #' of features used for each split in each tree for the RF
+  #' @param trees number of trees in the RF
+  #' @param s sample size 
   #' 
   #' @usage rolling_window_rf_walk_forward(data, window_size)
   #' 
@@ -32,45 +37,42 @@ rolling_window_rf_walk_forward <- function(data
   #' to be shifted according to the forecast horizon you are interested in.
   #' eg if you are interested in forecasting 3 steps ahead you have to make
   #' sure your data can be used to estimate the model 
-  #' y_{t+3} = F(x_{t}) + epsilon_{t+3} with x_{t} being features at time t
+  #' y_{t+3} = F(x_{t}) + epsilon_{t+3} with x_{t} being features at time t.
+  #' Omit the observations that have NA in the response due to the shifting.
   #' 
   #' @examples
-  #' rolling_window_rf_walk_forward(premium_data, 10000) creates a 
-  #' rolling window of size 10000
+  #' rolling_window_rf_walk_forward_ts(premium_data, 10000)
   
   
-  #set.seed(123)
   
   error <- c()
   predictions <- c()
+  colnames(data)[which(names(data) == response)] <- "response"
   
   for(i in 1:(nrow(data) - window_size)){
     
-    #set.seed(i)
+    window <- data[i:(i+window_size-1), ]
     
-    window <- data[i:(i+window_size)-1, ]
-    
-    rf_fit <- ranger(prem ~.
+    rf_fit <- ranger(response ~.
                      , data=window
-                     #, num.trees=200
-                     #, replace=FALSE
-                     #, sample.fraction=0.632
-                     #, mtry=mtry_split_amount
+                     , num.trees=trees
+                     , sample.fraction=s
+                     , mtry=mtry_split_amount
                      #, seed=i
                      )
     
     prediction <- predict(rf_fit ,
                           data=data[(i+window_size), ])
     
-    residual <- data[(i+window_size), "prem"] - prediction$predictions
+    residual <- data[(i+window_size), response] - prediction$predictions
     
     error <- c(error, residual)
     predictions <- c(predictions, prediction$predictions)
     
   }
+  error <- unname(unlist(error))
   msfe <- mean(error^2)
   
   return(list(error, msfe, rf_fit, predictions))
 }
 
-#docstring(rolling_window_rf_walk_forward)
